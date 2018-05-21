6e6d2b1771952d297080a6c433341d27aff266cb
Rafal Miodowski <rafalmiodowski@gmail.com>
2018-05-21 20:14:01 +0200

    Generate file type count report

--- files.rb
+++ files.rb
@@ -1,10 +1,15 @@
  directory_name = "discourse" 
 
- file_list = Dir.glob(File.join(directory_name, "**", "*.*")).inject({}) do |file|
+ file_list = Dir.glob(File.join(directory_name, "**", "*.*")).inject({}) do |hash, file|
  	ext = File.extname(file)
  	hash[ext] ||= 0
  	hash[ext] += 1
  	hash
  end
 
- puts file_list
+ File.open("report.txt", "w") do |file|
+ 	file_list.each do |file_type, count|
+ 		file.puts "#{file_type}\t#{count}"
+ 	end
+ end
+
