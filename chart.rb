8f11d134a2f1ee1bf313e3fe8bbf25868595ee1c
Rafal Miodowski <rafalmiodowski@gmail.com>
2018-05-21 20:24:42 +0200

    Create chart generating program

---
+++ chart.rb
@@ -0,0 +1,23 @@
+rows = File.open("report.txt", "r") { |file| file.readlines }.map { |row|
+row.chomp.split("\t") }.sort_by { |e| e.last.to_i }.reverse.take(10)
+
+
+puts rows
+
+
+=begin
+rescue Exception => e
+	
+end
+require "open-uri"
+
+rows = File.open("raport.txt", "r") { |file| file.readlines }.map { |e| e.chomp.split("\t")
+}.sort_by { |e| e.last.to_i }.reverse.take(10)
+
+values = rows.map { |r| r.last }.join(",")
+labels = rows.map { |r| r.first}.join("|")
+url = "https://chart.googleapis.com/chart?cht=p&chds=a&chd=t:#{values}&chs=950x375&chl=#{labels}"
+
+image_content = open(url).read
+File.open("raport.png", "w") { |f| f.write(image_content) }
+=end
